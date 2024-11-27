8828ad0fc609bfd95ba00a2cfe247057ef489002
Mari <mcramirez2223@gmail.com>
2024-11-27 19:24:20 +0100

    todos los datasets en .csv con columna 'Class' para el label

--- Pen-based/Preprocessing/preprocessing.py
+++ Pen-based/Preprocessing/preprocessing.py
@@ -1,5 +1,6 @@
 from Classes.Reader import DataPreprocessor
 import os
+import pandas as pd
 
 if __name__ == "__main__":
     dataset_path = '..'
@@ -8,5 +9,14 @@ else:
 
 arff_path = os.path.join(dataset_path, "../Datasets/pen-based.arff")
 complete_df = DataPreprocessor.load_arff(arff_path)
+
+# Initialize and fit the preprocessor on the training data and transform
+reader = DataPreprocessor(complete_df, class_column="a17")
+preprocessed_df = reader.fit_transform()
+
+# Renaming the last column
+preprocessed_df.rename(columns={preprocessed_df.columns[-1]: 'Class'}, inplace=True)
+
 preprocessed_path = os.path.join(dataset_path, "Preprocessing/pen-based.csv")
-complete_df.to_csv(preprocessed_path)
+preprocessed_df.to_csv(preprocessed_path)
+
