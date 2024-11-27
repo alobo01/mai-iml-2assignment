a6199267a9f8da2000922a4d786d4353c69bf6b1
brunosangom <brunosangom@gmail.com>
2024-11-27 12:50:58 +0100

    Pen-based KMeans and GlobalKMeans scripts

--- Pen-based/Results/globalkmeans_results.py
+++ Pen-based/Results/globalkmeans_results.py
@@ -1,26 +1,25 @@
 import os
 import time
 import pandas as pd
-import numpy as np
-from Classes.KMeans import KMeansAlgorithm
+from Classes.GlobalKMeans import GlobalKMeansAlgorithm
 from Classes.EvaluationUtils import EvaluationUtils
 
 if __name__ == "__main__":
     dataset_path = '..'
 else:
-    dataset_path = 'Hepatitis'
+    dataset_path = 'Pen-based'
 
 # Load dataset
-data_path = os.path.join(dataset_path, "Preprocessing/hepatitis.csv")
+data_path = os.path.join(dataset_path, "Preprocessing/pen-based.csv")
 data = pd.read_csv(data_path)
-class_labels = data['Class']
-X = data.drop(columns=['Unnamed: 0','Class']).values
+class_labels = data['a17']
+X = data.drop(columns=['Unnamed: 0','a17']).values
 
 # Define configurations to test
-max_k_value = 20
+max_k_value = 3
 distance_metrics = ['euclidean', 'manhattan', 'clark']
-max_iter = 10
-repetitions = 10
+max_iter = 1
+repetitions = 1
 
 # Initialize results DataFrame
 results = []
@@ -28,22 +27,19 @@ results = []
 # Perform tests
 for k in range(2, max_k_value+1):
     for _ in range(repetitions):
-        # Initialize random centroids by choosing k random samples
-        centroids = X[np.random.choice(X.shape[0], k, replace=False)]
-
         for distance_metric in distance_metrics:
 
             # Instantiate KMeans and measure performance
             start_time = time.time()
-            kmeans = KMeansAlgorithm(k, centroids.copy(), distance_metric, max_iter)
-            cluster_labels, E = kmeans.fit(X)
+            global_kmeans = GlobalKMeansAlgorithm(k, distance_metric, max_iter)
+            cluster_labels, E = global_kmeans.fit(X)
             end_time = time.time()
 
             # Evaluate clustering performance
             metrics = EvaluationUtils.evaluate(X, class_labels, cluster_labels)
             execution_time = end_time - start_time
 
-            algorithm = f'KMeans({k}, {distance_metric})'
+            algorithm = f'GlobalKMeans({k}, {distance_metric})'
             results.append({
                 'Algorithm': algorithm,
                 'E': E,
@@ -53,5 +49,5 @@ for k in range(2, max_k_value+1):
 
 # Save results to CSV file
 results_df = pd.DataFrame(results)
-csv_path = os.path.join(dataset_path, 'Results/CSVs/kmeans_results.csv')
+csv_path = os.path.join(dataset_path, 'Results/CSVs/global_kmeans_results.csv')
 results_df.to_csv(csv_path, index=False)
